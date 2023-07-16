c121d951abf3e6af48142faa28751741c4a6dbf3
Umair Abidi <56173806+umairabidi@users.noreply.github.com>
2023-07-16 03:12:16 -0400

    Working counter

--- MAX7219_main.c
+++ MAX7219_main.c
@@ -27,18 +27,21 @@
 #define DIN PORTCbits.RC2
 #define CS  PORTCbits.RC1
 #define CLK PORTCbits.RC0
-#define PULSETIME 50
+#define PULSETIME 1
+#define ON 1
+#define OFF 0
 
 void SPI_send(uint8_t addr, uint8_t data);
 void MAX7219_Setup();
-
-void turnOn(int digit_X);
-void decimal(int digit_X, int val);
-void blink(int digit_X);
+void display(int digit_X, int val);
+void display_decimal(int digit_X, int val);
+void blink(int digit_X, int frequency);
 void blank(int digit_X);
-void blankAll();
-void digTest();
-void setIntensity();
+void turnOff();
+void turnOn(int digit_X);
+void digTest(int state);
+void setIntensity(int intensity);
+
 
 /* Required Functions:
  * Illuminate digit X               turnOn(X)
@@ -54,26 +57,36 @@ void setIntensity();
 */
 
 int main(void){
-    
     TRISCbits.TRISC2 = 0;   // Set as output
     TRISCbits.TRISC1 = 0;   // Set as output
     TRISCbits.TRISC0 = 0;   // Set as output
     ANSEL = 0;              // All pins as Digital IO
-    
     CLK = 0;
     CS = 1;
     DIN = 0;
+    uint16_t i, temp;
+    uint8_t j;
+    MAX7219_Setup();
     
-    int i;
-    SPI_send(0x01,0x86);        // Digit
-    SPI_send(0x02,0x85);        // Digit
-    SPI_send(0x03,0x84);        // Digit
-    SPI_send(0x04,0x83);        // Digit
-    SPI_send(0x05,0x82);        // Digit
-    
-        
-    while(1){
+    for (i=0; i<1000000; i++){
+        temp = i;
+        for (j=0; j<8; j++){
+            display(j, temp%10);
+            temp/=10;
+        }
+        //__delay_ms(1);
     }
+    
+    display(1,1);
+    display(2,2);
+    display(3,3);
+    display(4,4);
+    display(5,5);
+    display(6,6);
+    display(7,7);
+    display(8,8);
+    
+    while(1);
     return 0;
 } 
 
@@ -82,20 +95,20 @@ void SPI_send(uint8_t addr, uint8_t data){
     CS = 0;
     for (i=0; i<8; i++){
         CLK = 0;
-        __delay_us(PULSETIME);
+        //__delay_us(PULSETIME);
         DIN = (addr & 0b10000000) ? 1:0 ;
-        __delay_us(PULSETIME);
+        //__delay_us(PULSETIME);
         CLK = 1;
-        __delay_us(PULSETIME);
+        //__delay_us(PULSETIME);
         addr = addr << 1;
     }
     for (i=0; i<8; i++){
         CLK = 0;
-        __delay_us(PULSETIME);
+        //__delay_us(PULSETIME);
         DIN = (data & 0b10000000) ? 1:0 ;
-        __delay_us(PULSETIME);
+        //__delay_us(PULSETIME);
         CLK = 1;
-        __delay_us(PULSETIME);
+        //__delay_us(PULSETIME);
         data = data << 1;
     }
     CLK = 0;
@@ -103,31 +116,37 @@ void SPI_send(uint8_t addr, uint8_t data){
 }
 
 void MAX7219_Setup(){
-    SPI_send(0x0F,0x00);        // Display Test
+    SPI_send(0x0F,0x00);        // Display normal operation
     SPI_send(0x09,0xFF);        // Decode mode ON
     SPI_send(0x0A,0x03);        // Intensity level
     SPI_send(0x0B,0x07);        // Scan-Limit
-    SPI_send(0x0C,0x01);        // Shutdown (turn it on)
-    
+    SPI_send(0x0C,0x01);        // Shutdown (turn it on)w   
 }
-void turnOn(int digit_X){
-    
+
+void display(int digit_X, int val){
+    SPI_send(digit_X, val);
 }
-void decimal(int digit_X, int val){
-    
+void display_decimal(int digit_X, int val){
+    SPI_send(digit_X, 0b10000000 | val);
 }
-void blink(int digit_X){
-    
+void blink(int digit_X, int frequency){
+    // Not sure how yet
 }
 void blank(int digit_X){
-    
+    SPI_send(digit_X, 0x0F);
 }
-void blankAll(){
-    
+void turnOff(){
+    SPI_send(0x0C,0x0F);
+    // Datasheet gives DIN=XXXXXXXX ?????
 }
-void digTest(){
-    
+void turnOn(int digit_X){
+    SPI_send(0x0C,0x01);
 }
-void setIntensity(){
-    
+void digTest(int state){
+    // If state is 0 or ON, turn off Display Test
+    // If state is 1 or OFF, turn on Display Test
+    SPI_send(0x0F, 0x00 | !state);
+}
+void setIntensity(int intensity){
+    SPI_send(0x0A, intensity);
 }
